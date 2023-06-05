a700cc91ae2e0fac3c1c9d5a59d37a9a30c4a642
Musharraf <musharrafalam9888@gmail.com>
2023-06-05 21:06:44 +0530

    cors origin updating for vercel client

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ dotenv.config({ path: './config.env'});
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://qr-info-client.vercel.app/"
 }));
 
 app.get("/", (req, res) => {
