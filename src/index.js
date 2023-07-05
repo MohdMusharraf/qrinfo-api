f5243fb216316063cba0d1aa1fe4234c4967b539
Mohd Musharraf <codejam9888@gmail.com>
2023-07-05 21:54:04 +0530

    cors for all

--- src/index.js
+++ src/index.js
@@ -12,12 +12,11 @@ dotenv.config({ path: './config.env'});
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+app.use(cors({
+    origin: ["http://localhost:3000", "https://qr-info-client.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
 
 app.get("/", (req, res) => {
     res.send("Hello to QR Code Generator API");
