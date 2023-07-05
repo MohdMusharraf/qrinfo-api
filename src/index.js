b090e61ff113d341a9ce26c9809cc8937afaf4d2
Mohd Musharraf <codejam9888@gmail.com>
2023-07-05 21:30:12 +0530

    cors setting

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ dotenv.config({ path: './config.env'});
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-    origin: ["https://qr-info-client.vercel.app", "http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://qr-info-client.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
 }));
