a8ae73e78f6613bac4edbb7e67a21a5c76946c7b
Mohd Musharraf <codejam9888@gmail.com>
2024-04-03 19:18:39 +0530

    cors setting 03

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import http from "http";
+import express from "express";
 import app from "./app.js";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
@@ -6,7 +7,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import QRCode from "qrcode";
 
+const app = express();
+app.use(express.json());
 app.use(cors());
+
 
 const server = http.createServer(app);
 
